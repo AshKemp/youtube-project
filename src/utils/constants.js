9028970d9ee629b32d0205e7207dfbe007eb34c1
AshKemp <kmuraliashwin@gmail.com>
2023-07-15 19:42:06 +0530

    build youtube

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY = "AIzaSyD_nfO4V_4FiXq-20sdleS6oRxswPtzrQM";
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
